e855f40d88e1861a3816c334a7d3343914eec4ed
robcio89 <robcio89@gmail.com>
2015-03-02 09:25:55 +0100

    working Pr1

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -4,7 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Answer: " << endl;
+    auto max = 0;
+    auto sum = 0;
+
+    cout << "Please enter a number: " << endl;
+    cin >> max;
+
+    for (auto i = 0; i < max; i++)
+    {
+       if (i %3 == 0 || i%5==0)
+           sum+=i;
+    }
+
+    cout << "Sum: " << sum << endl;
     return 0;
 }
 
